19204f9f3957060141547e4754c5a36bf1b35996
Anca Negulescu <anca.negulescu@validaide.com>
2025-12-13 16:41:17 +0100

    2025 day3 part2

--- 2025/day3/solution.php
+++ 2025/day3/solution.php
@@ -47,3 +47,48 @@ function solve_part1(string $input): int|string
 
     return $result;
 }
+
+function solve_part2(string $input): int|string
+{
+    $result = 0;
+
+    foreach (explode("\n", $input) as $line) {
+        $line = rtrim($line, "\r");
+        echo $line . "\n";
+
+        $array = str_split($line);
+        $maxes = array_fill(0, 12, 0);
+
+        $startIndex = 0;
+
+        for ($i = 0; $i < 12; $i++) {
+            $index = $startIndex;
+            $max   = 0;
+
+            // Stop early so there is still room for the remaining digits
+            /* Eg. 10 chars, picking 1st digit (i=0), to still have 5 in total we can go up to 10-5
+             * And the number would be made from digits at pos 5, 6, 7, 8, 9.
+             * Then if picking 3rd digit (i=2), it would be 10-(5-2)=7 and so digits 345 are at pos 7 8 9.
+             */
+            $endIndex = count($array) - (12 - $i);
+
+            for ($j = $startIndex; $j <= $endIndex; $j++) {
+                if ($array[$j] > $max) {
+                    $index = $j;
+                    $max   = $array[$j];
+                }
+                if ($max === 9) {
+                    // Can't get higher
+                    break;
+                }
+            }
+
+            $maxes[$i]  = $max;
+            $startIndex = $index + 1;
+        }
+
+        $result += (int)implode('', $maxes);
+    }
+
+    return $result;
+}
